ef97ea47e97e5fb724a0c2272c6910d4162e2aa5
Serhii Balbieko <serhii@balbieko.com>
2024-12-21 03:19:14 -0500

    Update .config/nvim/lua/plugins/onedark-theme.lua

--- dot_config/nvim/lua/plugins/onedark-theme.lua
+++ dot_config/nvim/lua/plugins/onedark-theme.lua
@@ -1,9 +1,10 @@
-return {
-  "navarasu/onedark.nvim",
-  lazy = false,
-  name = "onedark", 
-  priority = 1000,
-  config = function()
-    require('onedark').load()
-  end,
-}
+return {}
+-- return {
+--   "navarasu/onedark.nvim",
+--   lazy = false,
+--   name = "onedark", 
+--   priority = 1000,
+--   config = function()
+--     require('onedark').load()
+--   end,
+-- }
