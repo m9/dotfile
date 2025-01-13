bb2bd61b99974de767c92f22803670c3e397d1eb
Serhii Balbieko <serhii@balbieko.com>
2025-01-13 00:28:23 -0500

    Add .config/nvim/lua/plugins/conjure.lua

---
+++ dot_config/nvim/lua/plugins/conjure.lua
@@ -0,0 +1,11 @@
+return {
+  "Olical/conjure",
+  lazy = false,
+  name = "conjure",
+  dependencies = {
+    "guns/vim-sexp",
+    "tpope/vim-sexp-mappings-for-regular-people",
+    "tpope/vim-repeat",
+    "tpope/vim-surround",
+  }
+}
